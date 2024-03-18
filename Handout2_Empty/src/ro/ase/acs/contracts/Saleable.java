dc8cdfaf504fe722a88d48dff6e499a87ec63337
Serban Timofte <serbantimofte@MacBook-Pro-2.local>
2024-03-18 18:04:24 +0200

    Curs 5

---
+++ Handout2_Empty/src/ro/ase/acs/contracts/Saleable.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.contracts;
+
+public interface Saleable {
+    public abstract float getPrice();
+}
---
+++ Handout2_Empty/src/ro/ase/acs/main/Main.java
@@ -0,0 +1,11 @@
+package ro.ase.acs.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Playground
+		//here you can test your code manually
+
+	}
+
+}
